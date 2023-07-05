c2ca73a42d5eaca351a66dcb5cd6b753d89628e0
neek53 <nkhadka2050@gmail.com>
2023-07-05 13:20:54 -0500

    Init commit for intergration

---
+++ MavenGITIntergration/src/main/java/Itsutra/MavenGITIntergration/App.java
@@ -0,0 +1,13 @@
+package Itsutra.MavenGITIntergration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
